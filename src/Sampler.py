442945e8c40f0a4b35499c2a20c42a7c1146f796
unknown <saied.salem44@gmail.com>
2022-02-09 11:03:38 +0200

    add ing external signal functionalty and modifying ui

--- src/Sampler.py
+++ src/Sampler.py
@@ -1,11 +1,13 @@
 import numpy as np
+import pandas as pd
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore as qtc
 from PyQt5 import uic
+from PyQt5.QtWidgets import QFileDialog
 from scipy import signal
 
 from Sampling_and_reconstruction_canvas import Sampling_and_reconstruction_canvas
-
+from Fourier_package import get_f_max
 
 class Sampler(QtWidgets.QWidget):
 
@@ -21,6 +23,7 @@ class Sampler(QtWidgets.QWidget):
         self.Freq_rate_slider.setValue(10)
         self.Freq_rate_slider.valueChanged.connect(self.sample_signal)
         self.toggel_button.clicked.connect(self.toggel_visability_second_axes)
+        self.CSV_load_button.clicked.connect(self.load_external_signal)
 
     def clear_canvas(self):
         self.sampel_and_reco_canvas.clear_canvas()
@@ -46,11 +49,15 @@ class Sampler(QtWidgets.QWidget):
 
     def sample_signal(self):
         values = np.array(self.values)
+        print("inside sample")
+
         factor = (self.Freq_rate_slider.value()/10)
         self.Slider_lable.setText("{F_max}F-max".format(F_max = factor))
         sampling_freq =factor*self.max_freq
+        print("sampling_freq sample")
         sampled_time_points = np.arange(self.time[0],self.time[-1],1/sampling_freq)
 
+        print((self.values))
         sampled_values_points= values[np.searchsorted(self.time,sampled_time_points)]
 
         reconstructed_signal = self.reconstruct_signal(sampled_values_points,len(self.time))
@@ -74,3 +81,32 @@ class Sampler(QtWidgets.QWidget):
     def toggel_visability_second_axes(self):
         print("iiiiiiiiiiiiiiiii")
         self.sampel_and_reco_canvas.toggel_visability_second_axes()
+
+
+
+    def load_external_signal(self):
+        time , values =self.Load_csv_file()
+        self.time=time.values
+        self.values=values.values
+        # print(self.values)
+        self.max_freq = self.get_f_max_from_external_signal(time , values)
+        self.Lcd_max_freq.display(self.max_freq)
+        self.init_canvas()
+        self.plot_external_signal()
+
+    def Load_csv_file(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self, "Choose File", "", "csv (*.csv)",
+                                                      options=options)
+        df = pd.read_csv(fileName)
+        time = df.iloc[:, 0]
+        values = df.iloc[:, 1]
+        return  time , values
+
+    def get_f_max_from_external_signal(self,time,data):
+        max_freq=get_f_max(time,data)
+        return max_freq
+
+    def plot_external_signal(self):
+        self.sampel_and_reco_canvas.plot_composed_signal(self.time,self.values)
